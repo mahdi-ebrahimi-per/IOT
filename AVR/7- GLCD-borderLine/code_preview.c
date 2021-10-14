1975b61c0c30113bc62408eaf58a9c406c8582ca
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2021-10-14 20:04:04 +0330

    Create code_preview.c

---
+++ AVR/7- GLCD-borderLine/code_preview.c
@@ -0,0 +1,13 @@
+// main code is in `codeVision` directory (it's in zip file)
+// this file is just preview and doesn't work currectly.
+
+
+// modules : 
+
+
+
+// global var :
+
+
+
+// while code : 
