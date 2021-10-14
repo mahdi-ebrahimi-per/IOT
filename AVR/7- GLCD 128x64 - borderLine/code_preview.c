d8778ed3ed6ce92786ecdc23c427ca3c0a49f22c
Mahdi Ebrahimi <mahdiebi.exe@gmail.com>
2021-10-14 23:21:20 +0330

    Update code_preview.c

--- AVR/7- GLCD 128x64 - borderLine/code_preview.c
+++ AVR/7- GLCD 128x64 - borderLine/code_preview.c
@@ -16,62 +16,62 @@ int i;
 
 // upper while  : 
 
-glcd_clear();
+	glcd_clear();
 
-// up 
-for (i=0; i<128; i++){
-    glcd_putpixel(i, 0, 1);
-    delay_ms(50);
-}
-
-
-// right
-for (i=0; i<64; i++){
-    glcd_putpixel(127, i, 1);
-    delay_ms(50);
-}
-
-// down
-for (i=127; i>=0; i--){
-    glcd_putpixel(i, 63, 1);
-    delay_ms(50);
-}
+	// up 
+	for (i=0; i<128; i++){
+	    glcd_putpixel(i, 0, 1);
+	    delay_ms(50);
+	}
 
 
-// left
-for (i=63; i>=0; i--){
-    glcd_putpixel(0, i, 1);
-    delay_ms(50);
-}
+	// right
+	for (i=0; i<64; i++){
+	    glcd_putpixel(127, i, 1);
+	    delay_ms(50);
+	}
+
+	// down
+	for (i=127; i>=0; i--){
+	    glcd_putpixel(i, 63, 1);
+	    delay_ms(50);
+	}
 
 
-//______________________________
-
-// up 
-for (i=3; i<126; i++){
-    glcd_putpixel(i, 3, 1);
-    delay_ms(50);
-}
+	// left
+	for (i=63; i>=0; i--){
+	    glcd_putpixel(0, i, 1);
+	    delay_ms(50);
+	}
 
 
-// right
-for (i=3; i<61; i++){
-    glcd_putpixel(124, i, 1);
-    delay_ms(50);
-}
+	//______________________________
 
-// down
-for (i=124; i>=3; i--){
-    glcd_putpixel(i, 60, 1);
-    delay_ms(50);
-}
+	// up 
+	for (i=3; i<126; i++){
+	    glcd_putpixel(i, 3, 1);
+	    delay_ms(50);
+	}
 
 
-// left
-for (i=60; i>=3; i--){
-    glcd_putpixel(3, i, 1);
-    delay_ms(50);
-}
+	// right
+	for (i=3; i<61; i++){
+	    glcd_putpixel(124, i, 1);
+	    delay_ms(50);
+	}
+
+	// down
+	for (i=124; i>=3; i--){
+	    glcd_putpixel(i, 60, 1);
+	    delay_ms(50);
+	}
+
+
+	// left
+	for (i=60; i>=3; i--){
+	    glcd_putpixel(3, i, 1);
+	    delay_ms(50);
+	}
 
 
 // while code: 
