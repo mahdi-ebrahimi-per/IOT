7b8f09bc142b3c12cbcd5ed0d9c6b0143b455025
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2022-02-16 22:40:59 +0330

    Create 1.ino

---
+++ Arduino/LED-blinker/1.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int delayPeriod = 100;
+  digitalWrite(ledPin, HIGH);
+  delay(delayPeriod);
+  digitalWrite(ledPin, LOW);
+  delay(delayPeriod);
+
+    
+}
